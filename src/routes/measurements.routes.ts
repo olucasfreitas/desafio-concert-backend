607fb315db4cc4b3f41b2d3b3a1bd851167a873a
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-06-04 14:16:23 -0300

    adição de serviços para listagem e sincronização de medições

--- src/routes/measurements.routes.ts
+++ src/routes/measurements.routes.ts
@@ -1,16 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Measurement from '../models/Measurement';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import ReadCSVService from '../services/ReadCSVService';
 
 const measurementsRouter = Router();
 
 measurementsRouter.use(ensureAuthenticated);
 
-measurementsRouter.get('/', async (request, response) => {
-  return response.send();
+measurementsRouter.get('/', ensureAuthenticated, async (request, response) => {
+  const measurementsRepository = getRepository(Measurement);
+  const measurements = await measurementsRepository.find();
+  return response.json(measurements);
 });
 
-measurementsRouter.post('/', async (request, response) => {
-  return response.send();
+measurementsRouter.post('/', ensureAuthenticated, async (request, response) => {
+  const readCSV = new ReadCSVService();
+
+  const finalCSV = await readCSV.execute();
+
+  return response.json(finalCSV);
 });
 
 export default measurementsRouter;
