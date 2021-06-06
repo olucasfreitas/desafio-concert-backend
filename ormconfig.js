ab45f9053f8c50fd957e7982f948b4a44f50e31f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-06-06 18:39:11 -0300

    .

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 console.log('process.env.DATABASE_URL :>> ', process.env.DATABASE_URL);
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: 'postgres://otmaypuofwbsxb:49360bf472d95b536ab97fe8a9179e440f1840e829193eabaab47ca66d09ecb2@ec2-54-90-211-192.compute-1.amazonaws.com:5432/d8q8v47bsv9ib7',
   entities: ['dist/models/**/*.js'],
   migrations: ['src/database/migrations/**/*.js'],
   cli: {
